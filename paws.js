603c01b054ebca8dcbd2cfd26895305a4e5effce
jathom30 <jathom30@gmail.com>
2022-03-17 18:50:58 -0400

    init

---
+++ paws.js
@@ -0,0 +1,30 @@
+//hamburger tog
+
+const toggleButton = document.getElementById ('hamburger');
+const navLinks = document.getElementById('navLinks');
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+});
+
+function closeMenu() {
+    navLinks.classList.toggle ('active');
+}
+
+
+//function toggleMenu(params) {
+    //document.getElementById("navLinks").classList.toggle("open");
+//}
+//let x= document.getElementById('hamburgerBtn');
+//x.onclick= toggleMenu;
+
+
+//submit  name email
+
+const submitBtn = document.getElementById('btn');
+
+    submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    submitBtn.innerText = 'Submitted!';
+});
+
